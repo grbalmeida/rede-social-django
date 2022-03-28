d296234865c94e2dbb55a237abc01da865454bbc
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-03-27 23:28:54 -0300

    Views para alteração de senha

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    # urls para alteração de senha
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
+
+# A view PasswordChangeView cuidará do formulário de alteração de senha,
+# enquanto a view PasswordChangeDoneView exibirá uma mensagem de sucesso
+# depois que o usuário tiver alterado sua senha de forma bem-sucedida.
