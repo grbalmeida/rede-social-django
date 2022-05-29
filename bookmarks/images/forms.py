f48c8b19deb3e5970c429d30fa305f68f60c7f64
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-05-29 13:10:08 -0300

    Sobrescrevendo o método save do formulário ImageCreateForm

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,4 +1,7 @@
+from urllib import request
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from .models import Image
 
 class ImageCreateForm(forms.ModelForm):
@@ -18,3 +21,19 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError('The given URL does not match valid image extensions.')
         
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # faz o download da imagem a partir do URL especificado
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+
+        if commit:
+            image.save()
+        
+        return image
