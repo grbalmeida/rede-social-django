c57f094d3ee18ad8eb96c71c185fc9e93117b37c
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-05-31 19:30:03 -0300

    Listando imagens

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
@@ -61,3 +62,34 @@ def image_like(request):
             pass
 
         return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não for um inteiro, exibe a primeira página
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # Se a requisição for AJAX e a página estiver fora,
+            # do intervalo, devolve uma página vazia
+            return HttpResponse('')
+        
+        # Se a página estiver fora do intervalo,
+        # exibe a última página de resultados
+        images = paginator.page(paginator.num_pages)
+
+    if request.is_ajax():
+        # Para requisições AJAX, renderizamos o template list_ajax.html. Esse template
+        # conterá somente as imagens da página requisitada.
+        return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
+    
+    # Para requisições padrões, renderizamos o template list.html. Esse template
+    # estenderá o template base.html para exibir a página completa e incluirá o
+    # template list_ajax.html para incluir a lista de imagens.
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
